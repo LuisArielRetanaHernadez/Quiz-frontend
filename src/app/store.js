3440f565be7334eedf4c7430f42b55ee62129c92
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-04 17:57:35 -0600

    agreando el usersSclice en el archivo store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import usersSlice from '../features/users/usersSlice'
+
 export default configureStore({
   reducer: {
-
+    users: usersSlice
   }
 })
