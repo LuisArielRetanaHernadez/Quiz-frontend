d86b1d09a022b2d070a33c876568d865fc559b3b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-05 14:49:46 -0600

    creando la funcion reducer asyncThun loginAsync

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import authService from '../../utils/auth'
 
 const initialState = {
   token: '',
@@ -7,11 +9,34 @@ const initialState = {
   id: ''
 }
 
+export const loginAsync = createAsyncThunk(
+  'users/login',
+  async (data, thunkAPI) => {
+    try {
+      const response = await authService.login(data)
+      return response
+    } catch (error) {
+      return thunkAPI.rejectWithValue
+    }
+  }
+)
+
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
+  },
 
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginAsync.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(loginAsync.fulfilled, (state, action) => {
+        state.id = action.payload.token
+        state.isLogin = true
+        state.name = action.payload.name
+      })
   }
 })
 
