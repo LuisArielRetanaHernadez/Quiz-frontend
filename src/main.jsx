a86cd274cb60688d803b58f296d1ed983ea08e48
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-05 18:13:03 -0600

    creando el browserRouter con las rutas home, quizs y account. Tambien importamos el store para el rpovider del redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 // Redux
-import { Provider } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import store from './app/store'
+
+// views
+import Menu from './views/Menu.jsx'
+import Home from './views/Home.jsx'
+import Quizs from './views/Quizs.jsx'
+import Account from './views/Account.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+    children: [
+      {
+        path: 'Init',
+        element: <Home />
+      },
+      {
+        path: 'Quiz',
+        element: <Quizs />
+      },
+      {
+        path: 'Account',
+        element: <Account />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store=' '>
-      <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 )
