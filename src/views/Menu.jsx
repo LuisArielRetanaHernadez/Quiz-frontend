d2c55aa8d66045c2da605814b68d7eafbe6092dc
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-07 15:15:38 -0600

    agreando clases a los elementos

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,30 +1,31 @@
 import { Link, Outlet } from 'react-router-dom'
 
+import '../styles/views/Menu.css'
 const Menu = () => {
   return (
-    <section>
+    <section className='container-menu'>
 
-      <nav>
-        <ul>
-          <li>
+      <nav className='nav'>
+        <ul className='nav__list'>
+          <li className='nav__item'>
             <Link to='/Init'>Inicio</Link>
           </li>
-          <li>
+          <li className='nav__item'>
             <Link to='/Quiz'>Mis Quiz</Link>
           </li>
-          <li>
+          <li className='nav__item'>
             <Link to='/Create'>Crear</Link>
           </li>
-          <li>
+          <li className='nav__item'>
             <Link to='/Account'>Cuenta</Link>
           </li>
-          <li>
+          <li className='nav__item'>
             <Link to='/Login'>Login</Link>
           </li>
         </ul>
       </nav>
 
-      <section>
+      <section className='layout-menu'>
         <Outlet />
       </section>
     </section>
