9d0dc2db0367d096ebef88675453ab8ba28717c8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-07 15:20:36 -0600

    creando los elemento login por medio de un arreglo de objectos

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,48 @@
+const fieldsForm = [
+  {
+    input: {
+      name: 'password',
+      type: 'password',
+      placeholder: 'user@email.com',
+      value: ''
+    },
+    label: {
+      value: 'Correo'
+    }
+  },
+  {
+    input: {
+      name: 'email',
+      type: 'email',
+      placeholder: '*****',
+      value: ''
+    },
+    label: {
+      value: 'Contraseña'
+    }
+  }
+]
+
 const Login = () => {
+  const fields = fieldsForm.map((field, index) => (
+    <div key={index}>
+      <label>{field.label.value}</label>
+      <input
+        type={field.input.type}
+        placeholder={field.input.placeholder}
+      />
+    </div>
+  ))
+
   return (
-    <h2>Login</h2>
+    <div>
+      <form>
+        {fields}
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
