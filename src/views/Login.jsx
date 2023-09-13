bd4326c5a1c522411f073bfc87417b933cd93c20
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-13 14:28:14 -0600

    modificando la vista Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,48 +1,92 @@
+import { useForm } from 'react-hook-form'
+
+import Input from '../components/Input/Input'
+
+import '../styles/views/Login.css'
+import Label from '../components/Label/Label'
+import { Link } from 'react-router-dom'
+
 const fieldsForm = [
   {
     input: {
       name: 'password',
       type: 'password',
-      placeholder: 'user@email.com',
-      value: ''
+      placeholder: '*****',
+      className: 'login__input'
+    },
+    rules: {
+      required: true
     },
     label: {
-      value: 'Correo'
+      value: 'Correo',
+      className: 'login__label'
     }
   },
   {
     input: {
       name: 'email',
       type: 'email',
-      placeholder: '*****',
-      value: ''
+      placeholder: 'user@email.com',
+      className: 'login__input'
+    },
+    rules: {
+      required: true
     },
     label: {
-      value: 'Contraseña'
+      value: 'Contraseña',
+      className: 'login__label'
     }
   }
 ]
 
 const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
   const fields = fieldsForm.map((field, index) => (
-    <div key={index}>
-      <label>{field.label.value}</label>
-      <input
-        type={field.input.type}
-        placeholder={field.input.placeholder}
+    <div key={index} className='login__component'>
+      <Label
+        label={field.label.value}
+        className={field.label.className}
       />
+      <Input
+        key={index}
+        register={register}
+        errors={errors}
+        rules={field.rules}
+        attribute={field.input}
+        label={1}
+        class={field.input.className}
+      />
+      {errors[field.input.name] && <span>Error field</span>}
     </div>
   ))
 
   return (
-    <div>
-      <form>
-        {fields}
-        <div>
-          <button>Login</button>
+    <section className='container container--center'>
+
+      <div className='login login--transparent'>
+
+        <h2 className='title--scondary'>Iniciar Seccion</h2>
+
+        <form className='login__box border-box' onSubmit={handleSubmit()}>
+
+          {fields}
+
+          <div className='login__button m-0-5em'>
+            <button className='button w-100 border-box' type='Submit'>Iniciar</button>
+          </div>
+
+        </form>
+
+        <span>o</span>
+
+        <div className='login__extra'>
+          <Link to='/Init' className='link'>Registrar</Link>
         </div>
-      </form>
-    </div>
+
+      </div>
+
+    </section>
   )
 }
 
