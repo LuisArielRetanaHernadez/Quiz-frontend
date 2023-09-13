5e61b445c9b37fdefcc4c2b6ca8a408f47859bf1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-13 17:29:41 -0600

    Quitando el console.log()

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 const Input = ({ register, rules, attribute }) => {
-  console.log(rules)
   return (
     <input {...attribute} {...register(attribute.name, { ...rules })} />
   )
