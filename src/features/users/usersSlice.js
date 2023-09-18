f064ee9d2a63ccf106ba4b90eef413ef4d907349
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-18 15:51:06 -0600

    cambiando las estructura de los extraReducers del loginAsync

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -13,10 +13,12 @@ export const loginAsync = createAsyncThunk(
   'users/login',
   async (data, thunkAPI) => {
     try {
+      console.log('loginAsync')
       const response = await authService.login(data)
-      return response
+      return response.data
     } catch (error) {
-      return thunkAPI.rejectWithValue
+      console.log('error ', error)
+      return thunkAPI.rejectWithValue()
     }
   }
 )
@@ -32,10 +34,16 @@ const usersSlice = createSlice({
       .addCase(loginAsync.pending, (state) => {
         state.status = 'loading'
       })
+      .addCase(loginAsync.rejected, (state, action) => {
+        state.isLogin = false
+        state.status = 'fails'
+      })
       .addCase(loginAsync.fulfilled, (state, action) => {
-        state.id = action.payload.token
+        state.id = action.payload.user._id
         state.isLogin = true
-        state.name = action.payload.name
+        state.name = action.payload.user.name
+        state.token = action.payload.token
+        state.status = 'success'
       })
   }
 })
