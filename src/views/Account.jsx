14230c50335af5ad365ee2e28dd20639e701db08
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-19 13:56:02 -0600

    protegiendo la ruta Account

--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -1,4 +1,13 @@
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
 const Account = () => {
+  const isLogin = useSelector(state => state.users.isLogin)
+
+  if (!isLogin) {
+    return <Navigate to='/Login' />
+  }
+
   return (
     <h2>Account</h2>
   )
