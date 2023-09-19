6376c66d68bd25302c1dac581832c5c51808881f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-19 14:19:00 -0600

    agregando la comfiguracion de persistencia de redux persist en el store y los reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+
+// redux-persist
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import usersSlice from '../features/users/usersSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['users']
+}
+
+const rootReducer = combineReducers({
+  users: usersSlice
+})
+
+const persistRedux = persistReducer(persistConfig, rootReducer)
+
 export default configureStore({
-  reducer: {
-    users: usersSlice
-  }
+  reducer: persistRedux,
+  middleware: [thunk]
 })
