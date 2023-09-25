566dfbcf54015bcfa6019e7bf8805cd268cce3cc
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-25 13:56:06 -0600

    en el then solo devolvemos el response y no solo el data

--- src/utils/createRoom.js
+++ src/utils/createRoom.js
@@ -1,15 +1,9 @@
 import Axios from './Axios'
 
-// const token = localStorage.getItem('token')
-
-// Axios.defaults.headers.common = {
-//   Authorization: `Bearer ${token}`
-// }
-
 export const createRoom = data => {
   return Axios.post('room', {
     ...data
   })
-    .then(response => response.data)
+    .then(response => response)
     .catch(err => err)
 }
